b38e2d9f26edf10d78966bc7a6b8181eb47e7656
Le Minh Hung <leminhhungtabletennis@gmail.com>
2023-05-04 16:27:09 +0700

    Fixed bugs 4

--- utils/callApiService.js
+++ utils/callApiService.js
@@ -1,12 +1,16 @@
 import template from "./template.js";
 
-const getProductByTitle = (title) => {
+const getProductByTitle = async (title) => {
   const options = {
     uri: `${process.env.BASE_URL}/api/v1/products`,
     qs: { title, limit: 8 },
     method: "GET",
   };
-  return template.callApiTemplate(options);
+  try {
+    return await template.callApiTemplate(options);
+  } catch (error) {
+    console.log("error-call-api", error);
+  }
 };
 
 export default { getProductByTitle };
